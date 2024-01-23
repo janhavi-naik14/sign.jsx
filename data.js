d5be222d2747072e13939d3c1cc32998f4e60ed4
janhavi-naik14 <146354898+janhavi-naik14@users.noreply.github.com>
2024-01-24 01:44:30 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const imageSlide =[
+    {
+        url: 'slider.png',
+    },
+    {
+        url: 'logo192.png',
+    },
+    {
+        url: 'kitten.png',
+    },
+
+]
+
+export default imageSlide
