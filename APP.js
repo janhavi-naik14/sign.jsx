ce0aebe910ec4decd88e3bd59a9ac6261daffcc8
janhavi-naik14 <146354898+janhavi-naik14@users.noreply.github.com>
2024-01-24 17:27:43 +0530

    Create APP.js

---
+++ APP.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselPage from './CarouselPage';
+function App() {
+  return (
+    <div >
+      <CarouselPage/>
+    </div>
+  );
+}
+
+export default App;
